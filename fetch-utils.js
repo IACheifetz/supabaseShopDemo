c84d6d721789b2c3bc70903940c95a5c2330f81b
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-03-31 16:57:49 -0700

    fixed deleteParticipant function

--- fetch-utils.js
+++ fetch-utils.js
@@ -21,7 +21,7 @@ export async function deleteParticipant(id) {
         .from('participants')
         .delete()
         .match({ id: id })
-        .single;
+        .single();
 
     return checkError(response);
 }
